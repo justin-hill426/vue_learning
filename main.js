7c1ef6325486218a5623e443056d75ad7e289622
justin-hill426 <jrhill426@gmail.com>
2021-10-22 09:49:32 -0600

    Added component based approach

--- main.js
+++ main.js
@@ -1,32 +1,98 @@
-var app = new Vue({
-    el: '#app',
-    data: {
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template:
+    //html
+     `
+    <div class="product">
+        <div class="product-image">
+            <img v-bind:src="image">
+            </div>
+            <div class="product-info">
+            <h1>{{ product }}</h1>
+            <h3>{{ description }}</h3>
+            <p v-if="inStock">In Stock</p>
+            <p v-else :class="{strikeThrough: !inStock}">Out of Stock</p>
+            <p> Shipping is: {{ shipping }} </p>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <div v-for="(variant, index) in variants" 
+                :key="variant.variantId"
+                class="color-circle"
+                :style="{ backgroundColor: variant.variantColor }"
+                @mouseover="updateProduct(index)">
+            </div>
+
+            <div class="cart">
+                <p>Cart({{cart}})</p>
+            </div>
+
+            <button v-on:click="addToCart" 
+                    :disabled="!inStock"
+                    :class="{disabledButton: !inStock}">Add to Cart</button>
+        </div>
+    </div>`,
+  data() {
+      return {
+        brand: "Vue Mastery",
         user_cart: 0,
         product: 'Socks',
         description: 'This is a description for the item',
-        image: 'assets/images/socks_blue.jpg',
+        selectedVariant: 0,
         inventory: 26,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: 'assets/images/socks_green.jpg'
+                variantImage: 'assets/images/socks_green.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: 'assets/images/socks_blue.jpg'
+                variantImage: 'assets/images/socks_blue.jpg',
+                variantQuantity: 0
             }
         ],
         cart: 0 
+      }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + " " + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        shipping() {
+            if(this.premium) {
+                return "Free";
+            }
+            else {
+                return "$2.99"
+            }
         }
     }
+
+})
+var app = new Vue({
+    el: '#app',
 })
