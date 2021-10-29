ccec5503ecabcf5486e126020971e95d3a6e0bdb
justin-hill426 <jrhill426@gmail.com>
2021-10-29 10:08:43 -0600

    Added eventsBus for grandchildren event emissions

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var eventsBus = new Vue()
+
 Vue.component('product', {
     props: {
         premium: {
@@ -33,8 +35,11 @@ Vue.component('product', {
                     :disabled="!inStock"
                     :class="{disabledButton: !inStock}">Add to Cart</button>        
         </div>
-        <product-review></product-review>
-    </div>`,
+
+        <product-tabs :reviews="reviews"></product-tabs>
+    </div>
+
+        `,
   data() {
       return {
         brand: "Vue Mastery",
@@ -57,7 +62,7 @@ Vue.component('product', {
                 variantQuantity: 0
             }
         ],
-        cart: 0 
+        reviews: []
       }
     },
     methods: {
@@ -66,7 +71,7 @@ Vue.component('product', {
         },
         updateProduct(index) {
             this.selectedVariant = index
-        }
+        },
     },
     computed: {
         title() {
@@ -86,6 +91,11 @@ Vue.component('product', {
                 return "$2.99"
             }
         }
+    },
+    mounted() {
+        eventsBus.$on('submitted', productReview => {
+            this.reviews.push(productReview);
+        })
     }
 
 })
@@ -93,14 +103,109 @@ Vue.component('product-review', {
     template: 
     //html
     `
-        <input v-model="name">
+        <form class = "review-form" @submit.prevent="onSubmit">
+        <p v-if="errors.length">
+            <b> Please correct the following errors(s): </b>
+            <ul>
+                <li v-for="error in errors">
+                    *{{ error }}*
+                </li>
+            </ul>
+        <p>
+            <label for="name"> Name: </label>
+            <input id="name" v-model="name">
+        </p>
+
+        <p>
+            <label for="review">Review:</label>
+            <textarea id="review" v-model="review"></textarea>
+        </p>
+
+        <p>
+            <label for="rating">Rating:</label>
+            <select id="rating" v-model.number="rating">
+                <option>5</option>
+                <option>4</option>
+                <option>3</option>
+                <option>2</option>
+                <option>1</option>
+            </select>
+        </p>
+
+        <p>
+            <input type="submit" value="Submit">
+        </p>
+        </form>
     `,
     data() {
         return {
-            name: null
+            name: null,
+            review: null,
+            rating: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating
+                }
+                eventsBus.$emit('submitted', productReview);
+                this.name = null;
+                this.review = null;
+                this.rating = null;
+            }
+            else {
+                this.errors = []
+                if(!this.name) this.errors.push("Name Required")
+                if(!this.review) this.errors.push("Review Required")
+                if(!this.rating) this.errors.push("Rating Requrired")
+            }
         }
     }
 })
+
+Vue.component('product-tabs', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true,
+        }
+    },
+    template:  //html 
+    `  
+    <div>
+        <span class="tab" :class="{ activeTab: selectedTab === tab }" v-for="(tab, index) in tabs" :key="index" @click="selectedTab = tab">
+            {{ tab +  " " }} 
+        </span>
+
+        <div v-show="selectedTab === 'Reviews'">
+            <h2> Reviews </h2>
+            <p v-if="!reviews.length"> There are no reviews yet. </p>
+            <ul>
+                <li v-for="review in reviews">
+                    <p> Name: {{ review.name }} </p>
+                    <p> Rating: {{ review.rating }} </p>
+                    <p> Review: {{ review.review }} </p>
+                </li>
+            </ul>
+        </div>
+        <product-review v-show="selectedTab === 'Make a Review'"></product-review>
+
+    </div>
+    `,
+    data() {
+        return {
+            tabs: ['Reviews', 'Make a Review'],
+            selectedTab: 'Reviews'
+        }
+    }
+})
+
+
 var app = new Vue({
     el: '#app',
     data: {
